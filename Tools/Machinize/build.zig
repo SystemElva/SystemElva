d4c677dc7c3322b3d730815b9b60516053567c37
Lena Ickhorn <ericp.ickhorn@gmail.com>
2025-04-09 15:37:22 +0200

    tools/machinize: Add Zig build structure
    As Machinize will be written in Zig, the build system for Zig projects
    had to be added. Actual implementation source files don't exist yet.
    
    The root .gitignore now also contains the Zig build-files.

---
+++ Tools/Machinize/build.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn build(build_process: *std.Build) void {
+    const target = build_process.standardTargetOptions(.{});
+    const optimize = build_process.standardOptimizeOption(.{});
+
+    const executable = build_process.addExecutable(.{
+        .name = "Machinize",
+        .root_source_file = build_process.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    build_process.installArtifact(executable);
+    const run_command = build_process.addRunArtifact(executable);
+    run_command.step.dependOn(build_process.getInstallStep());
+
+    if (build_process.args) |args| {
+        run_command.addArgs(args);
+    }
+
+    const run_step = build_process.step("run", "Execute the Machinize assembler");
+    run_step.dependOn(&run_command.step);
+
+    const executable_unit_tests = build_process.addTest(.{
+        .root_source_file = build_process.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_executable_unit_tests = build_process.addRunArtifact(executable_unit_tests);
+
+    const test_step = build_process.step("test", "Run all unit tests");
+    test_step.dependOn(&run_executable_unit_tests.step);
+}
