b6b038dc20a624f236dcf643e201fc1b91a779e3
Lena Ickhorn <leickh@proton.me>
2025-04-22 17:23:30 +0200

    boot/i686-bios: Replace do.sh with src-sh/do.sh
    The 'do.sh'  in the i686  bootloader's  directory was  no more  than a
    redirect to the 'do.sh' within the src-sh directory for some time now.
    Now, the original file is removed/replaced.

--- System/Bootloader/i686-bios/do.sh
+++ System/Bootloader/i686-bios/do.sh
@@ -2,6 +2,33 @@
 
 # Ensure that the work-directory is the i686/ - direcetory
 cd $(dirname $0)
+I686_PATH=$(pwd)
 
-bash "src-sh/do.sh" "$@"
+if [[ "$#" == "0" ]];
+then
+    echo "Usage:   $ ./do.sh <action> [flags]"
+    echo "Example: $ ./do.sh help"
+    exit -1
+fi
+
+display_help() {
+    # If the general help-page is meant
+    if [[ "$#" == "0" ]];
+    then
+        cat "$I686_PATH/assets/help-texts/main.txt"
+        return
+    fi
+}
+
+case $1 in
+    "b" | "build")
+        $I686_PATH/src-sh/build.sh ${@:2:"$#"}
+        ;;
+    "q" | "qemu")
+        $I686_PATH/src-sh/run-qemu.sh ${@:2:"$#"}
+        ;;
+    "h" | "help")
+        display_help ${@:2:"$#"}
+        ;;
+esac
 
