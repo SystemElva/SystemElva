05161d1656ddd680808c2a61e2e7387c397803f2
Lena Ickhorn <leickh@proton.me>
2025-04-23 15:17:19 +0200

    boot/i686-bios: Add more memory utilities
    Contents of a memory region can now be set to a specific value and can
    be copied to another location.

--- System/Bootloader/i686-bios/modules/code-partition/src-asm/utility.asm
+++ System/Bootloader/i686-bios/modules/code-partition/src-asm/utility.asm
@@ -288,3 +288,74 @@ memory_equal:
     popad
     ret
 
+
+
+; memory_copy:
+;   Copy the content of one memory region to another.
+;
+; Arguments:
+;   [FURTHEST FROM EBP]
+;     2.  U32       len_regions     (only lower byte used)
+;     1.  Ptr32     source
+;     0.  Ptr32     destination
+;  [NEAREST TO EBP]
+;
+; Return Value:
+;   N/A
+memory_copy:
+.prolog:
+    pushad
+
+.setup_copy_loop:
+    mov esi, [ebp - 8]
+    mov edi, [ebp - 4]
+    mov edx, [ebp - 12]
+    xor ecx, ecx
+
+.copy_loop:
+    cmp ecx, edx
+    jae .epilog
+
+    mov al, [esi + ecx]
+    mov [edi + ecx], al
+
+    inc ecx
+    jmp .copy_loop
+
+.epilog:
+    popad
+    ret
+
+
+
+; memory_set:
+;   Set a whole memory region's bytes to one value.
+;
+; Arguments:
+;   [FURTHEST FROM EBP]
+;     2.  U16       value           (only lower byte used)
+;     1.  U32       len_region
+;     0.  Ptr32     region
+;  [NEAREST TO EBP]
+;
+; Return Value:
+;   N/A
+memory_set:
+.prolog:
+    pushad
+
+.setup_filler_loop:
+    xor ecx, ecx
+    mov ebx, [ebp - 4]
+    mov al, [ebp - 10]
+
+.filler_loop:
+    cmp ecx, [ebp - 8]
+    jae .epilog
+    mov [ebx + ecx], al
+    inc ecx
+    jmp .filler_loop
+
+.epilog:
+    popad
+    ret
