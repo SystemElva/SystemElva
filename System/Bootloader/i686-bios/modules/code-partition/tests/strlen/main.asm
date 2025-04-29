372426294524767c89c40ba1c8d220eb9644af85
Lena Ickhorn <leickh@proton.me>
2025-04-29 06:46:59 +0200

    boot/i686-bios: Add test for string_Length
    The utility function 'string_length' now has a test.

--- System/Bootloader/i686-bios/modules/code-partition/tests/strlen/main.asm
+++ System/Bootloader/i686-bios/modules/code-partition/tests/strlen/main.asm
@@ -1,55 +1,71 @@
 
 bits 16
 org 0xa000
-entry:
-    push dword text.unimplemented
-    jmp crash_with_text
+main:
+.setup_stack:
+    mov ebx, 0x800
+    mov ss, ebx
+    mov ebp, 0x2000
+    mov esp, ebp
 
-text:
-.unimplemented:
-    db "Unimplemented!", 0x00
+    sub esp, 256
+    mov esi, esp
 
-; crash_with_text:
-;   @todo
-; 
-; Arguments:
-;   [FURTEHST FROM STACK-TOP]
-;     0.  Ptr32     string_pointer
-;   [NEAREST TO STACK-TOP]
-; 
-; Return Value:
-;   N/A
-crash_with_text:
-    ; Reset/Clear display
+.clear_display:
     mov ah, 0x00
     mov al, 0x03
     int 0x10
 
-    pop ebx
+.setup_string_loop:
+    xor     ecx,                ecx
+
+.string_loop:
+    mov     edx,                ecx
+    shl     edx,                3
+    add     edx,                4 ; Go to string pointer
+    mov     ebx,                [text_pointers + edx]
+
+    ; If this is the last entry, all was successfull.
+    cmp     ebx,                0
+    jne     .continue
+
 
     push ebp
     mov ebp, esp
-    push ebx
-    push dword 80
-    call string_length
+    push word BACKGROUND_GREEN
+    call set_background
     mov esp, ebp
     pop ebp
 
-    ; Subtract half the string's size from half the screen's width (80 / 2 = 40)
-    ; to get the start position of the error message.
-    shr eax, 1
-    mov edx, 40
-    sub edx, eax
-
     push ebp
     mov ebp, esp
-    push ebx
-    push dword edx
+    push dword status_codes.correct
+    push dword 36
     push dword 12
     push word FOREGROUND_WHITE
     call write_text
     mov esp, ebp
     pop ebp
+
+    cli
+    hlt
+
+.continue:
+    push    ebp
+    mov     ebp,                esp
+    push    ebx
+    call    string_length
+    mov     esp,                ebp
+    pop     ebp
+
+    mov     edx,                ecx
+    shl     edx,                3
+    mov     ebx,                [text_pointers + edx]
+
+    inc     ecx
+    cmp     ebx,                eax
+    je      .string_loop
+
 
     push ebp
     mov ebp, esp
@@ -58,8 +74,26 @@ crash_with_text:
     mov esp, ebp
     pop ebp
 
+    push ebp
+    mov ebp, esp
+    push dword status_codes.invalid
+    push dword 36
+    push dword 12
+    push word FOREGROUND_WHITE
+    call write_text
+    mov esp, ebp
+    pop ebp
+
     cli
     hlt
 
-%include "utility.asm"
 
+
+status_codes:
+.invalid:
+    db "Invalid", 0x00
+.correct:
+    db "Correct", 0x00
+
+%include "utility.asm"
+%include "generated-test-cases.asm"
