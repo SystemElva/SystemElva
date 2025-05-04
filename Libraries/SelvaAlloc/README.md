aa6054eec07091214b4896181d93dff792f3998c
Lena Ickhorn <leickh@proton.me>
2025-05-04 22:18:10 +0200

    libs/selva-alloc: Add project build structure
    The SelvaAlloc allocator now has the basic build structure.

---
+++ Libraries/SelvaAlloc/README.md
@@ -0,0 +1,4 @@
+# SelvaAlloc
+
+SystemElva's  `std.heap.PageAllocator` - based arbitrary  memory  size
+allocator. Currently work-in-progress.
---
+++ Libraries/SelvaAlloc/build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(build_process: *std.Build) void {
+    const target = build_process.standardTargetOptions(.{});
+
+    const optimize = build_process.standardOptimizeOption(.{});
+
+    const lib_mod = build_process.createModule(.{
+        .root_source_file = build_process.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = build_process.addLibrary(.{
+        .linkage = .static,
+        .name = "SelvaAlloc",
+        .root_module = lib_mod,
+    });
+
+    build_process.installArtifact(lib);
+
+    const lib_unit_tests = build_process.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = build_process.addRunArtifact(lib_unit_tests);
+
+    const test_step = build_process.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
